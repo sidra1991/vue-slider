8cc22d99c1a0f99fb7f45a542488006da7ac0dfb
sidra1991 <1991sidra@gmail.com>
2022-03-13 20:09:14 +0100

    creati oggetti

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+const slider = new Vue ({
+    el:"#root",
+    data: {
+        slide =[ 
+        {
+            title:'Svezia',
+            items: 'img/01.jpg',
+            text : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+        },
+        {
+            title:'img/02.jpg',
+            items:'Svizzera',
+            text :'Lorem ipsum',
+        },
+        {
+            title:'img/03.jpg',
+            items:'Gran Bretagna',
+            text :'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        },
+        {
+            title:'img/04.jpg',
+            items:'Germania',
+            text :'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+        },
+        {
+            title:'img/05.jpg',
+            items:'Paradise',
+            text :'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+        },
+        ]
+    }
+
+
+})
